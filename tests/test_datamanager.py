0460d2803ab79ab2b3d1e1ade189b7363c8bf8c0
Ruggero Turra <giurrero@gmail.com>
2014-03-11 23:02:20 +0100

    fix test

--- tests/test_datamanager.py
+++ tests/test_datamanager.py
@@ -1,7 +1,7 @@
 import unittest
 import numpy as np
 from numpy import nan
-from ..DataManager import DataManager, Mean
+from bad.DataManager import DataManager, Mean
 
 class TestDataManager(unittest.TestCase):
     def setUp(self):
