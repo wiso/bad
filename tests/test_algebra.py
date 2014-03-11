ebcc412a6dcbc43d5f43747b56d9861239207c7e
Ruggero Turra <giurrero@gmail.com>
2014-03-11 21:43:30 +0100

    add test algebra

---
+++ tests/test_algebra.py
@@ -0,0 +1,8 @@
+import unittest
+from bad.algebra import *
+from bad.algebra import _0, _1, _2, _3, _4, _N
+
+class TestAlgebra(unittest.TestCase):
+    def test_simplify_constants(self):
+        self.assertEqual((_1 + _2).simplify_constants(), _3)
+        self.assertEqual((_1 + _2 + _3).simplify_constants(), _N(6))
